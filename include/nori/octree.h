4bc58224bf818626cad30946abda4fb398bb79e6
k03302 <unisonman660@gmail.com>
2025-07-21 17:19:49 +0900

    Octree on progress

--- include/nori/octree.h
+++ include/nori/octree.h
@@ -1,71 +1,133 @@
 #pragma once
 #include <nori/common.h>
-
+#include <nori/bbox.h>
 NORI_NAMESPACE_BEGIN
 
-template <typename T> class OctNode
+class NodeAlloc nodeAlloc;
+
+struct Element
+{
+    int index;
+    Mesh* mesh;
+};
+
+class OctNode
 {
 public:
     const static int CHILD_COUNT = 8;
     const static int ELEMENT_COUNT = 10;
     
-    OctNode* push(const T& element)
+    bool isLeaf()
     {
-        if(elementNo < ELEMENT_COUNT)
+        return bIsLeaf;
+    }
+
+    OctNode* push(const int& element)
+    {
+        if(isLeaf())
         {
-            elements[elementNo++] = element;
-            return this;
+            if(elementNo < ELEMENT_COUNT)
+            {
+                elements[elementNo++] = element;
+                return this;
+            }
+            else
+            {
+                createChildren();
+                pushToChildren(element);
+                
+                bIsLeaf = false;
+            }
         }
         else
         {
-            for(int i = 0; i < CHILD_COUNT; i++)
+            pushToChildren(element);
+        }
+    }
+
+    void createChildren()
+    {
+        for(int i = 0; i < 2; i++)
+        {
+            for(int j = 0; j < 2; j++)
             {
-                nodes[i] = 
+                for(int k = 0; k < 2; k++)
+                {
+                    int index = 4 * i + 2 * j + k;
+
+                    OctNode* newNode = nodeAlloc.getNode();
+                    const Vector3d diagonal = bbox.max - bbox.min;
+                    const Vector3d childDiagonal = diagonal / 2.0;
+                    const Vector3i childPosVector(i, j, k);
+
+                    const Point3d newMin = childDiagonal * childPosVector;
+                    const Point3d newMax = newMin + childDiagonal;
+                    BoundingBox3d newBbox(newMin, newMax);
+
+                    children[index] = newNode;
+                }
             }
         }
     }
 
-    
-    
+    void pushToChildren(const int& element)
+    {
+        for(int i = 0; i < 2; i++)
+        {
+            for(int j = 0; j < 2; j++)
+            {
+                for(int k = 0; k < 2; k++)
+                {
+                    int index = 4 * i + 2 * j + k;
 
-    OctNode() = 0;
-    OctNode(const BoundingBox3d& _bbox, int _depth, shared_ptr<NodeAllocator> _allocator)
-        : bbox(_bbox), depth(_depth), elementNo(0), allocator(_allocator)
+                    OctNode* newNode = nodeAlloc.getNode();
+                    const Vector3d diagonal = bbox.max - bbox.min;
+                    const Vector3d childDiagonal = diagonal / 2.0;
+                    const Vector3i childPosVector(i, j, k);
+
+                    const Point3d newMin = childDiagonal * childPosVector;
+                    const Point3d newMax = newMin + childDiagonal;
+                    BoundingBox3d newBbox(newMin, newMax);
+
+                    children[index] = newNode;
+                }
+            }
+        }
+
+        for(int i = 0; i < 8; i++)
+        {
+            if (children[i] == nullptr) continue;
+            children[i]->push(element);
+        }
+    }
+
+    OctNode() = default;
+    OctNode(const BoundingBox3d& _bbox, int _depth)
+        : bbox(_bbox), depth(_depth), elementNo(0), bIsLeaf(true)
     {}
 
 private:
     BoundingBox3d bbox;
-    OctNode<T>* nodes[CHILD_COUNT];
-    T elements[ELEMENT_COUNT];
+    OctNode* children[CHILD_COUNT];
+    int elements[ELEMENT_COUNT];
     int depth;
     int elementNo;
-    shared_ptr<NodeAllocator> allocator;
+    bool bIsLeaf;
 };
 
-template <typename T> class NodeAllocator
+class NodeAlloc
 {
-public:
-    const static int NODE_COUNT = 1000000;
-
-    NodeAllocator()
-    {
-        initialize();
-    }
-
-    void initialize()
-    {
-        nodeNo = 0;
-    }
-
-    OctNode<T>* newNode()
-    {
-        return &nodePool[nodeNo++];
-    }
-
 private:
-    OctNode<T> nodePool[NODE_COUNT];
-    int nodeNo = 0;
-};
+    const static int TOTAL_NODE_COUNT = 1000000;
+    int nodeCount;
+    OctNode nodePool[TOTAL_NODE_COUNT];
+public:
+    NodeAlloc() : nodeCount(0) {}
+    OctNode* getNode()
+    {
+        return &nodePool[nodeCount++];
+    }
+} nodeAlloc;
 
 
 
