8cd9150534fd776af758b60dd5d9c7e682bdaca8
k03302 <unisonman660@gmail.com>
2025-08-05 21:14:05 +0900

    path_maths on progress

--- src/path_mats.cpp
+++ src/path_mats.cpp
@@ -1,5 +1,9 @@
 #include <nori/integrator.h>
 #include <nori/scene.h>
+#include <nori/emitter.h>
+#include <nori/sampler.h>
+#include <nori/bsdf.h>
+#include <nori/frame.h>
 
 NORI_NAMESPACE_BEGIN
 
@@ -13,21 +17,99 @@ public:
 
     Color3f Li(const Scene *scene, Sampler *sampler, const Ray3f &ray) const
     {
-        /* Find the surface that is visible in the requested direction */
-        Intersection its;
-        if (!scene->rayIntersect(ray, its))
-            return Color3f(0.0f);
+        Ray3f last_ray = ray;
+        Intersection last_its;
+        float eta = 1.0f;
+        Color3f last_reflectance(1.0f);
+        Color3f throughput(1.0f);
+        Color3f resultColor(0.0f);
 
-        /* Return the component-wise absolute
-           value of the shading normal as a color */
-        Normal3f n = its.shFrame.n.cwiseAbs();
-        return Color3f(n.x(), n.y(), n.z());
+        for (int iteration = 0;; ++iteration)
+        {
+            if (iteration >= 3)
+            {
+                if (sampler->next1D() < std::fmin(0.99, eta * eta * throughput.maxCoeff()))
+                {
+                    break;
+                }
+            }
+
+            if (!scene->rayIntersect(last_ray, last_its))
+            {
+                throughput = Color3f(0.0f);
+                break;
+            }
+
+            if (const Emitter *emitter = last_its.mesh->getEmitter())
+            {
+                const Ray3f emittingRay = Ray3f(last_its.p, -last_ray.d);
+                resultColor += throughput * emitter->Le(emittingRay);
+            }
+
+            if (const BSDF *bsdf = last_its.mesh->getBSDF())
+            {
+                BSDFQueryRecord bRec(last_its.shFrame.toLocal(-last_ray.d));
+                last_reflectance = bsdf->sample(bRec, sampler->next2D());
+                throughput *= last_reflectance;
+                last_ray = Ray3f(last_its.p, last_its.shFrame.toWorld(bRec.wo));
+                eta *= bRec.eta;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        // if (throughput.isZero() || last_its.mesh == nullptr)
+        // {
+        //     return resultColor;
+        // }
+
+        // const BSDF *bsdf = last_its.mesh->getBSDF();
+        // if (bsdf != nullptr && bsdf->isDiffuse())
+        // {
+        //     for (const auto &m : scene->getMeshes())
+        //     {
+        //         if (!m->isEmitter())
+        //             continue;
+
+        //         const Emitter *emitter = m->getEmitter();
+
+        //         Point3f p;
+        //         Vector3f n;
+        //         emitter->sampleSurface(sampler->next2D(), p, n);
+
+        //         // Incident vector
+        //         Vector3f incident = p - last_ray.o;
+        //         float distance = incident.norm();
+        //         Vector3f incidentDir = incident / distance;
+
+        //         // Check if sampled point is occluded
+        //         Ray3f shadowRay(p, -incidentDir);
+        //         shadowRay.maxt = distance - Epsilon;
+        //         if (scene->rayIntersect(shadowRay))
+        //             continue;
+
+        //         // Reflection vector
+        //         Vector3f reflect = ray.o - last_its.p;
+        //         Vector3f reflectDir = reflect.normalized();
+
+        //         Frame itsFrame(n);
+        //         BSDFQueryRecord query(itsFrame.toLocal(incidentDir), itsFrame.toLocal(reflectDir), ESolidAngle);
+        //         throughput *= bsdf->eval(query);
+        //         resultColor += throughput * emitter->Le(shadowRay);
+        //     }
+        // }
+
+        return resultColor;
     }
 
     std::string toString() const
     {
         return "PathMatsIntegrator[]";
     }
+
+private:
 };
 
 NORI_REGISTER_CLASS(PathMatsIntegrator, "path_mats");
